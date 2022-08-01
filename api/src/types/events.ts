5ea03e44017c3051f7e0ab82c3ad6021b3ba6614
MARX Dominic <dominic.marx@axians.de>
2022-08-01 08:40:06 +0200

    remove directus stuff from workbench

--- api/src/types/events.ts
+++
@@ -1,8 +0,0 @@
-import { ActionHandler, FilterHandler, InitHandler } from '@directus/shared/types';
-import { ScheduledTask } from 'node-cron';
-
-export type EventHandler =
-	| { type: 'filter'; name: string; handler: FilterHandler }
-	| { type: 'action'; name: string; handler: ActionHandler }
-	| { type: 'init'; name: string; handler: InitHandler }
-	| { type: 'schedule'; task: ScheduledTask };
