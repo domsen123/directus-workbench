4942a3dc9d3138eb830f9775bf2dce6a9ed99d73
MARX Dominic <dominic.marx@axians.de>
2022-08-05 11:49:11 +0200

    reset.sh

--- workbench/__misc/reset.sh
+++ workbench/__misc/reset.sh
@@ -1,16 +1,12 @@
 git add -A .
 git commit -m "before reset"
 
-git checkout main
-git fetch upstream main
-git reset --hard upstream/main
+git checkout main && git fetch upstream main && git reset --hard upstream/main
 git checkout workbench
 
-rm -rf ./workbench/_instance/extensions/**/*
-rm -rf app
-rm -rf api
-rm -rf packages
 
-git checkout main -- api
-git checkout main -- app
-git checkout main -- packages
+rm -rf ./workbench/_instance/extensions/**/* && rm -rf app && rm -rf api && rm -rf packages
+
+git checkout main -- api && git rm -r --cached --ignore-unmatch api
+git checkout main -- app && git rm -r --cached --ignore-unmatch app
+git checkout main -- packages && git rm -r --cached --ignore-unmatch packages
